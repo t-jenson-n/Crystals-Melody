8562bcb6163aa153de166b9643a52c51316abfbe
ejfkwhflw <portia.s.bridge@gmail.com>
2026-03-04 23:57:23 -0500

    playfunction

--- Assets/Scripts/AudioPlay.cs
+++ Assets/Scripts/AudioPlay.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class AudioPlay : MonoBehaviour
 {
+    public GameObject[] parentbars; //parent
+    private GameObject[] bars = new GameObject[24]; //child
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < 4; i++)
+        {
+            for(int j=0; j < 6; j++)
+            {
+                //bars[(i*6)+j] = parentbars[i].transform.GetChild(j).gameObject;
+                int val = (i * 6) + j;
+                bars[val] = parentbars[i].transform.GetChild(j).gameObject;
+                //Debug.Log("val"+val+": "+parentbars[i].transform.GetChild(j).gameObject.name);
+            }
+        }
         
+       /* for(int i = 0;i < bars.Length; i++)
+        {
+            Debug.Log(i+": "+bars[i].name+", ");
+        }*/
     }
 
     // Update is called once per frame
