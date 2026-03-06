82721a7b89d2606fcab3eb4075a634ae5b64ddb8
Petri124 <portia.s.bridge@gmail.com>
2026-03-06 01:21:36 -0500

    Connecting music controller

--- Assets/Scripts/scriptsMusicUI/hover_effect.cs
+++ Assets/Scripts/scriptsMusicUI/hover_effect.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,22 +9,29 @@ public class hover_effect : MonoBehaviour
     private AudioSource[] sounds;
     public GameObject note;
     private Renderer barRenderer;
+    public GameObject[] barindx;
+
+
     // Start is called before the first frame update
     void Start()
     {
+
+        barindx = new GameObject[4] { GameObject.Find("CBar (0)"), GameObject.Find("CBar (1)"), GameObject.Find("GBar (2)"), GameObject.Find("GBar (3)") };
         sounds = GetComponentsInParent<AudioSource>();
         barRenderer = GetComponent<Renderer>();
+
     }
     void OnTriggerEnter(Collider collision)
     {
         //EditorUtility.DisplayDialog("working?", "working", "ok", "cancel");
         if (collision.transform.parent.gameObject == note)
         {
-            barRenderer.material.color = collision.GetComponent<Renderer>().material.color ;
+            barRenderer.material.color = collision.GetComponent<Renderer>().material.color;
             if (collision.CompareTag(this.gameObject.tag))
             {
                 // Debug.Log("right");
                 sounds[1].Play();
+                gameObject.SendMessageUpwards("checkCorrect", Array.IndexOf(barindx, this.gameObject));
             }
             else
             {
@@ -42,6 +50,11 @@ public class hover_effect : MonoBehaviour
     private void OnTriggerExit(Collider collision)
     {
         barRenderer.material.color = Color.black;
-       
+
+        if (Array.IndexOf(barindx, this.gameObject) != -1)
+        {
+            gameObject.SendMessageUpwards("checkFalse", Array.IndexOf(barindx, this.gameObject));
+
+        }
     }
 }
