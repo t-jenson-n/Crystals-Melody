fd89be421cc5a195a4afc84373e7a8141990f2e3
aidanator1000 <126937635+aidanator1000@users.noreply.github.com>
2026-04-07 15:28:52 -0400

    Did a buncha shit

---
+++ Assets/Scripts/VidPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+//Got this bit from https://www.youtube.com/watch?v=mIK9EcwwEMY
+public class VidPlayer : MonoBehaviour
+{
+    [SerializeField] string videoFileName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayVideo();
+    }
+
+
+    public void PlayVideo()
+    {
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+
+        if (videoPlayer)
+        {
+            string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
+            Debug.Log(videoPath);
+            videoPlayer.url = videoPath;
+            videoPlayer.Play();
+        }
+    }
+}
--- Assets/Scripts/scriptsMusicUI/dragdrop.cs
+++ Assets/Scripts/scriptsMusicUI/dragdrop.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Experimental.AI;
 using UnityEngine.UIElements;
